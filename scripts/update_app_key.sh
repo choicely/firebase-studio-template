02b3f901bba5a09c9e4609db5ba3d95577c80c19
Casimir <casimir@choicely.com>
2026-01-19 16:38:58 +0200

    Disable apk patching

--- scripts/update_app_key.sh
+++ scripts/update_app_key.sh
@@ -3,24 +3,24 @@ set -euo pipefail
 
 NEW_APP_KEY="${CHOICELY_APP_KEY}"
 
-raw_name=${CHOICELY_APP_NAME:-}
-lower_name=$(printf '%s\n' "$raw_name" | tr '[:upper:]' '[:lower:]')
-safe_app_name=${lower_name//[^a-z0-9_-]/-}
-apk_path=./out/apk/"$safe_app_name".apk
-
-if [ -f "$apk_path" ]; then
-  echo "APK already exists at: $apk_path"
-else
-  ./scripts/android/patch_apk.sh \
-    "https://github.com/choicely/choicely-sdk-demo-react-native/releases/download/v0.0.9-alpha/choicely-rn.apk" \
-    "${NEW_APP_KEY}" \
-    "$apk_path" &
-fi
+#raw_name=${CHOICELY_APP_NAME:-}
+#lower_name=$(printf '%s\n' "$raw_name" | tr '[:upper:]' '[:lower:]')
+#safe_app_name=${lower_name//[^a-z0-9_-]/-}
+#apk_path=./out/apk/"$safe_app_name".apk
+#
+#if [ -f "$apk_path" ]; then
+#  echo "APK already exists at: $apk_path"
+#else
+#  ./scripts/android/patch_apk.sh \
+#    "https://github.com/choicely/choicely-sdk-demo-react-native/releases/download/v0.0.9-alpha/choicely-rn.apk" \
+#    "${NEW_APP_KEY}" \
+#    "$apk_path" &
+#fi
 
 export QR_CODE_PATH=./res/preview/qr.png
 ./scripts/utils/make_qr.sh "https://studio.choicely.link/?ctype=app&ckey=${NEW_APP_KEY}&link=https%3A%2F%2Fchoicely.com%2F" "$QR_CODE_PATH"
 #./scripts/create_apk_qr.sh
-wait
+#wait
 
 #while :; do
 #  sleep 1
