5b8fee0b6c4cde6f589ee96fc20ce4f39422a201
Casimir <casimir@choicely.com>
2026-01-28 13:54:39 -0800

    Set env in idx-template

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -25,26 +25,6 @@
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         default.openFiles = [ ];
-        env-setup = ''
-          set -eo pipefail
-          PROJECT_DIR="$PWD"
-          pushd "$HOME"
-          cat > ~/.bashrc <<BASHRC
-          unset PROMPT_COMMAND
-          __vsc_prompt_cmd_original() { :; }
-          unset -f command_not_found_handle 2>/dev/null || true
-          # auto-export env vars from the original project dir
-          if [ -d "$PROJECT_DIR" ]; then
-            set -a
-            [ -f "$PROJECT_DIR/default.env" ] && . "$PROJECT_DIR/default.env"
-            [ -f "$PROJECT_DIR/.env" ] && . "$PROJECT_DIR/.env"
-            set +a
-          fi
-          chmod -R a+x $PROJECT_DIR/scripts
-          BASHRC
-          popd
-          exit
-        '';
       };
       # Runs when a workspace restarted
       onStart = {
